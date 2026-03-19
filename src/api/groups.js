8fc438bbdeef0df7d732d841d4b0da99873b5938
Louie Camacho <louiecamacho@gmail.com>
2026-03-19 18:46:13 -0500

    create api/groups

---
+++ src/api/groups.js
@@ -0,0 +1,75 @@
+const API = import.meta.env.VITE_API;
+
+export async function getGroups() {
+  try {
+    const response = await fetch(API);
+    const result = await response.json();
+    return result;
+  } catch (e) {
+    console.log('CANNOT GET GROUPS: ', e);
+  }
+}
+
+export async function getGroupById(id) {
+  try {
+    const response = await fetch(API+id);
+    const result = await response.json();
+    return result;
+  } catch (e) {
+    console.log('CANNOT GET GROUP BY ID: ', e);
+  }
+}
+
+export async function getGroupByUserId(userId) {
+  try {
+    const response = await fetch(API+userId);
+    const result = await response.json();
+    return result;
+  } catch (e) {
+    console.log('CANNOT GET GROUP BY USER ID: ', e);
+  }
+}
+
+export async function createGroup(token, name, creatorId) {
+  const response = await fetch(API + "/groups", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+    body: JSON.stringify({ name, creatorId }),
+  });
+  const result = await response.json();
+  if (!response.ok) throw Error(result.message);
+  return result;
+}
+
+export async function joinGroup(token, id) {
+  const response = await fetch(API + "/groups", {
+    method: "UPDATE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+    body: JSON.stringify({ id }),
+  });
+  const result = await response.json();
+  if (!response.ok) throw Error(result.message);
+  return result;
+}
+
+export async function leaveGroup(token, id) {
+  if (!token) {
+    throw Error("You are not signed in.");
+  }
+
+  const response = await fetch(API + "/groups/" + id, {
+    method: "DELETE",
+    headers: { Authorization: "Bearer " + token },
+  });
+
+  if (!response.ok) {
+    const result = await response.json();
+    throw Error(result.message);
+  }
+}
