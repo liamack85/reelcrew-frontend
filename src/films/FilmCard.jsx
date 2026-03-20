0d540cc2b735818842b2b88d639a025509f0e60f
liamack <liamack85@gmail.com>
2026-03-20 16:02:49 -0400

    Add FilmDetail page, link to details from filmcard, route in App

--- src/films/FilmCard.jsx
+++ src/films/FilmCard.jsx
@@ -1,9 +1,14 @@
 import { addToWatchlist } from "../api/films";
+import { useState } from "react";
+import { Link } from "react-router";
 
 export default function FilmCard({ film, token }) {
+  const [added, setAdded] = useState(false);
+
   const handleAddToWatchlist = async () => {
     try {
       await addToWatchlist(token, film.id);
+      setAdded(true);
     } catch (e) {
       console.error(e);
     }
@@ -12,12 +17,18 @@ export default function FilmCard({ film, token }) {
   return (
     <li>
       <img src={film.poster_url} alt={film.title} />
-      <h2>{film.title}</h2>
+      <Link to={"/films/" + film.id}>
+        <h2>{film.title}</h2>
+      </Link>
       <h3>{film.genre}</h3>
       <h3>{film.year}</h3>
       <h3>{film.director}</h3>
       {film.rating && <p>⭐ {film.rating}</p>}
-      {token && <button onClick={handleAddToWatchlist}>+ Watchlist</button>}
+      {token && (
+        <button onClick={handleAddToWatchlist} disabled={added}>
+          Add to Watchlist
+        </button>
+      )}
     </li>
   );
 }
