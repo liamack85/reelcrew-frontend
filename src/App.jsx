f5e70d225c7e5078d38f25adc50d1492d5f43b63
Robbie Sanders <sande290@gmail.com>
2026-03-20 12:27:10 -0400

    add splashpage and routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Layout from "./layout/Layout";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import ProfilePage from "./profile/ProfilePage";
+import SplashPage from "./layout/SplashPage";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route index element={<p>Home page</p>} />
+        <Route index element={<SplashPage />} />
         <Route path="/users/me" element={<ProfilePage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
