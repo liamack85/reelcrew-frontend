54da1be5f0c267cd92da3ad87f75fdcf6a4f2144
Louie Camacho <louiecamacho@gmail.com>
2026-03-21 11:48:46 -0500

    fix splash page ref

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Layout from "./layout/Layout";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import ProfilePage from "./profile/ProfilePage";
-import SplashPage from "./layout/SplashPage";
+import SplashPage from "./splashPage/SplashPage";
 import FilmsPage from "./films/FilmsPage";
+import GroupsPage from "./groups/GroupsPage";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<SplashPage />} />
+        <Route path="/groups" element={<GroupsPage />} />
         <Route path="/films" element={<FilmsPage />} />
         <Route path="/users/me" element={<ProfilePage />} />
         <Route path="/register" element={<Register />} />
