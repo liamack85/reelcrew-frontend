5b8a68ad9f08ce06a5778cf93606627938184dd0
Robbie Sanders <sande290@gmail.com>
2026-03-23 10:48:13 -0400

    add MUI components to Sidebar

--- src/splashPage/SplashPage.jsx
+++ src/splashPage/SplashPage.jsx
@@ -1,3 +1,23 @@
+import "./SplashPage.css";
+
 export default function SplashPage() {
-  return <p>SplashPage</p>;
+  return (
+    <section>
+      <p>REELCREW - WATCH TOGETHER</p>
+      <p>Watch together, on time.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A,
+        aspernatur!
+      </p>
+      <div>
+        <button>Start Watch Group</button>
+        <button>Browse Films</button>
+      </div>
+      <div id="stat-line">
+        <p>Stat1</p>
+        <p>Stat2</p>
+        <p>Stat3</p>
+      </div>
+    </section>
+  );
 }
