6848535bf39988789b3fe2ef4d031c0b7a3927f4
Louie Camacho <louiecamacho@gmail.com>
2026-03-24 14:24:15 -0500

    display group members

--- src/groups/GroupDetails.jsx
+++ src/groups/GroupDetails.jsx
@@ -1,28 +1,46 @@
-import { getGroupById } from "#src/api/groups";
-import { Box } from "@mui/material";
+import { getGroupById, getMembers } from "#src/api/groups";
+import { Box, Chip } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router"
 
 export default function GroupDetails() {
   const {id} = useParams();
-  const [groupDetail, setGroupDetail] = useState(null);
+  const [group, setGroup] = useState(null);
+  const [groupMembers, setGroupMembers] = useState(null);
   
   const syncGroupDetail = async() => {
-    const data = await getGroupById(id);
-    setGroupDetail(data);
+    const group = await getGroupById(id);
+    const groupMembers = await getMembers(id);
+    console.log(groupMembers);
+    
+    setGroup(group);
+    setGroupMembers(groupMembers);
   }
   useEffect(()=>{
     syncGroupDetail();
   }, [id]);
 
-  if(!groupDetail) return <p>Loading...</p>;
+  if(!group) return <p>Loading...</p>;
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column" }}>
       <h1>Group Details</h1>
       <Link to="/groups">back to groups</Link>
-      <p>Group Name: {groupDetail.name}</p>
-      <p>Creator: {groupDetail.creator_id}</p>
+      <p>Group Name: {group.name}</p>
+      <p>Creator: {group.creator_id}</p>
+      <p>Description: blah blah blah</p>
+      
+      <ul>
+      {groupMembers.map((member)=>(
+        <li key={member.id}>
+          <p>{member.username}
+          {member.role === "host" ? 
+          <Chip label={member.role} size="small" sx={{ mt: 0.5 }} />
+          : null}
+          </p>
+        </li>
+      ))}
+      </ul>
     </Box>
   )
 }
