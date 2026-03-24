95dbc50f57545424d33b5d3584efcfeccfa0daaa
Louie Camacho <louiecamacho@gmail.com>
2026-03-24 14:57:53 -0500

    add some mui styling

--- src/groups/GroupDetails.jsx
+++ src/groups/GroupDetails.jsx
@@ -1,5 +1,5 @@
 import { getGroupById, getMembers } from "#src/api/groups";
-import { Box, Chip } from "@mui/material";
+import { Box, Chip, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router"
 
@@ -23,11 +23,10 @@ export default function GroupDetails() {
   if(!group) return <p>Loading...</p>;
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "column" }}>
-      <h1>Group Details</h1>
+    <Box sx={{ display: "flex", flexDirection: "column", padding: 1 }}>
+      <Typography variant="h1">Group Details</Typography>
       <Link to="/groups">back to groups</Link>
-      <p>Group Name: {group.name}</p>
-      <p>Creator: {group.creator_id}</p>
+      <Typography variant="h2">{group.name} <button>Join</button><button>Leave Group</button></Typography>
       <p>Description: blah blah blah</p>
       
       <ul>
@@ -38,6 +37,7 @@ export default function GroupDetails() {
           <Chip label={member.role} size="small" sx={{ mt: 0.5 }} />
           : null}
           </p>
+
         </li>
       ))}
       </ul>
