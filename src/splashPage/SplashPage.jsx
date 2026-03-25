19e3b4526168098a1ca61d0f7737027446f207aa
Robbie Sanders <sande290@gmail.com>
2026-03-25 17:57:37 -0400

    remove excess import

--- src/splashPage/SplashPage.jsx
+++ src/splashPage/SplashPage.jsx
@@ -1,7 +1,6 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
-import TopBar from "../layout/TopBar";
 import Typography from "@mui/material/Typography";
 import { useAuth } from "../auth/AuthContext";
 import { useNavigate } from "react-router";
