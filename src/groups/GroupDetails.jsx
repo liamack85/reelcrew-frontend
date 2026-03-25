649bb7e363c38db8b985f6b00d282c59ecec4fc0
Louie Camacho <louiecamacho@gmail.com>
2026-03-25 14:29:57 -0500

    pass correct params

--- src/groups/GroupDetails.jsx
+++ src/groups/GroupDetails.jsx
@@ -1,6 +1,7 @@
-import { getGroupById, getMembers } from "../api/groups";
+import { getGroupById, getMembers, joinGroup } from "../api/groups";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router";
+import { useAuth } from "../auth/AuthContext";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Chip from "@mui/material/Chip";
@@ -11,9 +12,11 @@ import WatchPageList from "../watches/WatchPageList";
  * @returns {JSX.Element}
  */
 export default function GroupDetails() {
+  const {token, user} = useAuth();
   const { id } = useParams();
   const [group, setGroup] = useState(null);
   const [groupMembers, setGroupMembers] = useState(null);
+  const [joining, setJoining] = useState(false);
 
   /**
    * Fetch group details and members from the API and update component state.
@@ -30,13 +33,27 @@ export default function GroupDetails() {
 
   if (!group) return <p>Loading...</p>;
 
+  // handler for user action JOIN
+  const joinCurrentGroup = async () => {
+    try {
+      await joinGroup(token, id, user.id, "member"); // perform join      
+      setJoining(true);
+      await syncGroupDetail(); // refresh group and members
+    } catch (err) {
+      console.error('Join failed:', err);
+    }
+    setJoining(false);
+  };
+
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column", padding: 1 }}>
       <Typography variant="h1">Group Details</Typography>
       <Link to="/groups">back to groups</Link>
       <Link to={"/watch-group/" + id + "/watches"}>View watch events</Link>
       <Typography variant="h2">
-        {group.name} <button>Join</button>
+        {group.name} 
+        <button onClick={joinCurrentGroup}>Join</button>
         <button>Leave Group</button>
       </Typography>
       <p>
