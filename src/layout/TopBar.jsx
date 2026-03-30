d4430e9666a54645efea9498ae4e0a4e4ea61c74
Louie Camacho <louiecamacho@gmail.com>
2026-03-30 12:24:28 -0500

    add logo

--- src/layout/TopBar.jsx
+++ src/layout/TopBar.jsx
@@ -1,6 +1,7 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import Logo from "../images/rc_logo.svg";
 
 /** Fixed app bar displayed at the top of every page. Uses elevated zIndex to render above the Sidebar drawer. */
 export default function TopBar() {
@@ -11,7 +12,7 @@ export default function TopBar() {
       sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
       <Toolbar>
         <Typography variant="h6" noWrap component="div">
-          REEL CREW
+          <img src={Logo} alt="Reel Crew"/>REEL CREW
         </Typography>
       </Toolbar>
     </AppBar>
